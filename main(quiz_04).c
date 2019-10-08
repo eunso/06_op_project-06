ef20334e5065ef468659366b05e70b5c217e76b4
eunso <55428135+eunso@users.noreply.github.com>
2019-10-08 15:48:50 +0900

    5주차 프린트 실습 04

---
+++ main(quiz_04).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int i=0, meter;
+	while (i<3)
+	{
+		meter = i*1609;
+		printf("%d mile is %d meter.\n",i, meter);
+		i++;
+	}
+	
+	return 0;
+}
